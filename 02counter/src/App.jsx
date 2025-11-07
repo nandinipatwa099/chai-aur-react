6e29e551fd5848d27932449f2b01738215ce14f6
Nandini <nandinipatwa573@gmail.com>
2025-11-07 17:12:32 +0530

    upadate

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,23 +4,38 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  
-  let counter = 5;
+
+  let [counter, setCounter ] = useState(5) 
+
+  // let counter = 5;
 
   const addValue = () => {
-    console.log("value added", Math.random());
+    // console.log("value added", Math.random());
+    console.log("clicked", counter);   // UI UPDATION PROBLEM - control by react
+    // counter = counter + 1;
+    setCounter(counter + 1);
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1);
   }
   
+
+
   return (
     <>
       <h1>chai aur react</h1>
       <h2>Counter value: {counter}</h2>
 
-      <button onClick={addValue}>Add value</button>
+      <button onClick={addValue}>Add value {counter}</button>
       <br />
-      <button>Decrease value</button>
+      <button onClick={removeValue}>Decrease value {counter}</button>
+      <p>footer: {counter}</p>
     </>
   )
 }
 
-export default App
+
+export default App   
+
+
