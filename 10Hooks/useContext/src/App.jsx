ba1e4510a9b59010b13587b0529f7b9893630a7a
Nandini <nandinipatwa573@gmail.com>
2025-11-26 14:17:40 +0530

    ToggleTheme usecontext API small App

--- 10Hooks/useContext/src/App.jsx
+++ 10Hooks/useContext/src/App.jsx
@@ -4,20 +4,45 @@ import ChildA from './components/ChildA';
 
 // Step1: create context
  const userContext = createContext();
+
 // Step2: wrap all child inside a provider 
 // Step3: pass value
 // Step4: consumer ke andr jakr consume krlo
 
+
+const ThemeContext = createContext();
+
 function App() {
   const [user, setuser] = useState({name: 'Kush'});
+  const [theme , setTheme] = useState("light");
 
   return (
     <>
-     <div style={{fontWeight:'bold' , color:'darkcyan', fontSize:'20px'}}>useContext() with CodeHelp.in </div>
+ 
+    <userContext.Provider value={user}>
+      <ThemeContext.Provider value={{theme, setTheme}}>
+
+      <div id='container' style={{backgroundColor:theme==='light'?"bisque":"black"}} >
+        <ChildA /> 
+      </div>
+
+      {/* //hierarchy add  //25 */}
+      
+    </ThemeContext.Provider>
+
+    </userContext.Provider>
+    
+
+
+
+
+
+
+     {/* <div style={{fontWeight:'bold' , color:'darkcyan', fontSize:'20px'}}>useContext() with CodeHelp.in </div>
 
      <userContext.Provider value={user}>
        <ChildA />
-     </userContext.Provider>
+     </userContext.Provider> */}
     
     </>
   )
@@ -25,4 +50,5 @@ function App() {
 
 export default App
 export {userContext}
+export {ThemeContext}
 
--- 10Hooks/useContext/src/components/ChildC.jsx
+++ 10Hooks/useContext/src/components/ChildC.jsx
@@ -1,14 +1,41 @@
 import React, { useContext } from 'react'
-import { userContext } from '../App'  //import first
+// import { userContext } from '../App'  //import first
+
+import { ThemeContext, userContext } from '../App'
 
 
 function ChildC() {
   const user = useContext(userContext);
+
+  const {theme, setTheme} = useContext(ThemeContext);
+  function ToggleTheme() {
+    if(theme === 'light')
+      setTheme('dark')
+    else
+      setTheme('light')
+
+  }
   return (
+    <>
     <div>
-      Hello <br></br>
+      <h4 style={{color:"red"}}>Hello</h4> <br />
+      {/* theme: {theme}  */}
+      
+      <button onClick={ToggleTheme}>
+        Change Theme
+      </button>
+      
+        {/* Hello <br />
+        data : {user.name} */}
+    </div>
+
+
+
+    <div>
+      Hello <br />
       data : {user.name}
     </div>
+    </>
   )
 }
 
