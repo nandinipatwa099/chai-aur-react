92f36586d6c65ea53faff292637b9a2e3b291fe2
Nandini <nandinipatwa573@gmail.com>
2026-01-12 13:30:21 +0530

    integrate json server api and loader

---
+++ 17IntegrateJSON_serverAPI/src/App.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [userdata, setuserdata] = useState([]);
+  const [loading , setloading] = useState(false);
+
+  useEffect(() => {
+    // debugger
+    setloading(true);
+
+    getUserData();
+  }, []);
+
+  const getUserData = async () => {
+    // debugger
+    try{
+      //risky code
+       const url = "http://localhost:4000/users";
+    let response = await fetch(url);
+    response = await response.json();
+    console.log(response);
+    setuserdata(response);
+
+    setloading(false);
+    }
+    catch(err){
+      // handle error
+      console.log("Error occured while fetching data ", err);
+      setloading(false);
+
+    }
+    
+  };
+  console.log(userdata);
+
+  return (
+    <>
+      <h2>Integrate JSON Server API</h2>
+       <ul className="user-list user-list-head">
+          <li>Name </li>
+          <li>Age</li>
+          <li>Email</li>
+        </ul>
+
+      {
+      !loading?
+      userdata.map((user) => (
+        <ul key={user.name} className="user-list">
+          <li>{user.name} </li>
+          <li>{user.age}</li>
+          <li>{user.email}</li>
+        </ul>
+      ))
+      : <h2>Data Loading...</h2>
+    
+    }
+    </>
+  );
+}
+
+export default App;
